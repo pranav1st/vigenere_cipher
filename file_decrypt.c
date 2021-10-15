5445cd37c76fe99b37ab6cd68f32bb6b1705f172
pranav1st <pranavm14002@gmail.com>
2021-10-15 18:01:05 +0530

    File Decryption - #5
    Modified condition to consider spaces and reads line till new-line character is encountered
    
    Added decryption of text from file with encrypted text

--- file_decrypt.c
+++ file_decrypt.c
@@ -20,8 +20,10 @@ void file_decryption(char key[])
         int ciphertext_len = 0;
         for (int i = 0; (ch = fgetc(f1)) != EOF; i++)
         {
-            if ((ch >= 'A') && (ch <= 'Z'))
+            if (((ch >= 'A') && (ch <= 'Z')) || (ch==' '))
                 ciphertext_len++;
+            if (ch=='\n')
+                break;
         }
         //printf("cipher text length: %d\n", ciphertext_len);
 
@@ -38,7 +40,20 @@ void file_decryption(char key[])
         newKey[i] = '\0';
         printf("\nNew Generated Key: %s\n", newKey);
 
-        
+        char decrypted_msg[ciphertext_len];
+        char text[ciphertext_len];
+        fseek(f1, 0, SEEK_SET);
+        fscanf(f1, "%[^\n]", text);
+        //converting cipher text to plain text character wise using vigenere cipher algorithm
+        for(i = 0; i < ciphertext_len; ++i)
+        {
+            if (text[i]!=' ')
+                decrypted_msg[i] = ((((text[i] - newKey[i]) + 26) % 26) + 'A');
+            else
+                decrypted_msg[i]=' ';   //to print spaces
+        }
+        decrypted_msg[i] = '\0'; //denote the end of a string
+        printf("\nDecrypted Message: %s", decrypted_msg);
         
     }
     fclose(f1);
